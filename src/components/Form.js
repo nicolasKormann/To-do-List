3f62d4c7aa23154d8f8d428dbc75e6e10645e8d0
Nícolas Kormann <kormann.nicolas@gmail.com>
2022-10-25 12:46:24 -0300

    feat: input focus when click button

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,7 +1,9 @@
 import { Button, Input } from "dracula-ui";
-import React, { useState } from "react"
+import React, { useState, useRef } from "react"
 
 function Form(props) {
+
+  const input = useRef(null);
   const [text, setText] = useState("")
 
   function hadleChange(event) {
@@ -14,12 +16,14 @@ function Form(props) {
     if (text) {
       props.onAddItem(text)
       setText('')
+      input.current.focus();
     }
   }
+  
 
   return (
     <form>
-      <Input color="purple" size="lg" placeholder="Add a task" type='text' onChange={hadleChange} value={text} autoFocus></Input>
+      <Input ref={input} color="purple" size="lg" placeholder="Add a task" type='text' onChange={hadleChange} value={text} autoFocus></Input>
       <Button type="submit" as="button" size="lg" onClick={addItem}>Add</Button>
     </form>
   )
